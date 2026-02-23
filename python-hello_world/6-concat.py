11a422eca1093dca857b49c48545afafb68b5310
mmehemmeddd <muhammad.mammdv@gmail.com>
2026-02-23 13:06:12 +0000

    task6

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str1="Welcome to"
-str2="Holberton School!"
-print(f"{str1}")
-print(f"{str2}")
+str1 = "Welcome to"
+str2 = "Holberton School!"
+str1 = f"{str1} {str2}"
+print(str1)
