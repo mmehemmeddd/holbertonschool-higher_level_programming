774c40ee243d471ec280f0a07f1d83ca414a57d1
mmehemmeddd <muhammad.mammdv@gmail.com>
2026-03-05 12:40:36 +0000

    task0

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
     for char in str:
-        # Convert to ASCII number
         ascii_val = ord(char)
-        
-        # Check if it's in the lowercase range (a-z)
         if 97 <= ascii_val <= 122:
-            # Subtract 32 to get the uppercase version
             char = chr(ascii_val - 32)
-        
         print("{}".format(char), end="")
-    print("")  # This prints the required new line at the very end
+    print("")
