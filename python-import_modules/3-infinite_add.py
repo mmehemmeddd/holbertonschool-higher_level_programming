d78e6ca09837b191d060208d1763f7159c1d1eda
mehemmeddd <muhammad.mammdv@gmail.com>
2026-03-14 21:31:38 +0000

    tas

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -3,8 +3,6 @@ import sys
 
 if __name__ == "__main__":
     total = 0
-    # Skip the first element (script name) and loop through the rest
     for arg in sys.argv[1:]:
         total += int(arg)
-    
     print("{}".format(total))
