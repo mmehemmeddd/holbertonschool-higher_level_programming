9d848ea7f72463be0fe23cdc1286d6ebe6d6430e
mehemmeddd <muhammad.mammdv@gmail.com>
2026-03-21 09:56:22 +0000

    task0

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    """
+    Computes the square value of all integers of a matrix.
+    Returns a new matrix with the squared values, leaving the original unchanged.
+    """
+    # Create a new matrix using a nested list comprehension
+    return [[element ** 2 for element in row] for row in matrix]
