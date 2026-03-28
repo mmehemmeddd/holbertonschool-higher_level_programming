ebb857ca1b8328d59afbf2093ad82d05f2ab3499
mehemmeddd <muhammad.mammdv@gmail.com>
2026-03-28 16:05:20 +0000

    task0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-"""Module to safely print elements from a list."""
 
 
 def safe_print_list(my_list=[], x=0):
-    """Prints x elements of a list, safely handling out of bounds."""
     count = 0
-    for i in range(x):
-        try:
-            print("{}".format(my_list[i]), end="")
+    try:
+        for i in range(x):
+            print(my_list[i], end="")
             count += 1
-        except IndexError:
-            break
-    print("")
+    except IndexError:
+        pass
+    print()
     return count
