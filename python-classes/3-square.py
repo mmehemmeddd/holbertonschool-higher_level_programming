71697b46e7fcaa48e1760f6b62cff08c35ed7111
mehemmeddd <muhammad.mammdv@gmail.com>
2026-03-29 16:22:20 +0000

    task3 i solvem it

---
+++ python-classes/3-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This module defines a Square class with a private size attribute."""
+class Square :
+    """A class that defines a square."""
+    def __init__(self, size = 0) :
+        """Initialize the square with a specific size."""
+        if type(size) is not int :
+            raise TypeError("size must be an integer")
+        if size < 0 :
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    def area(self) :
+            
+            return self.__size * self.__size
