347b320af660804762a9ff0cde47f8f4cd3295be
mehemmeddd <muhammad.mammdv@gmail.com>
2026-04-03 06:31:08 +0000

    task6

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
-"""This module defines a Square class with size and position."""
+"""This module defines a Square class with size and position attributes."""
+
+
 class Square:
     """A class that defines a square."""
+
     def __init__(self, size=0, position=(0, 0)):
         """Initialize the square."""
         self.size = size
         self.position = position
+
     @property
     def size(self):
         """Retrieve the size."""
         return self.__size
+
     @size.setter
     def size(self, value):
         """Set the size."""
@@ -18,10 +23,12 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
     @property
     def position(self):
         """Retrieve the position."""
         return self.__position
+
     @position.setter
     def position(self, value):
         """Set the position."""
@@ -30,9 +37,11 @@ class Square:
                 value[0] < 0 or value[1] < 0):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
+
     def area(self):
         """Return the area of the square."""
         return self.__size * self.__size
+
     def my_print(self):
         """Print the square with the # character."""
         if self.__size == 0:
