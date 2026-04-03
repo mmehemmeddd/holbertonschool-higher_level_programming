c9838a9b15eee736c2fa300bf4781b9afa19ded0
mehemmeddd <muhammad.mammdv@gmail.com>
2026-04-03 06:29:39 +0000

    task5

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -1,23 +1,30 @@
 #!/usr/bin/python3
 """This module defines a Square class with a private size attribute."""
+
+
 class Square:
     """A class that defines a square."""
-    def __init__(self, size = 0):
+
+    def __init__(self, size=0):
         """Initialize the square with a specific size."""
         self.size = size
+
     @property
     def size(self):
         return self.__size
+
     @size.setter
     def size(self, value):
-        if type(value) is not int :
+        if type(value) is not int:
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-    def area(self) :
-            """Return the area of the square."""
-            return self.__size * self.__size
+
+    def area(self):
+        """Return the area of the square."""
+        return self.__size * self.__size
+
     def my_print(self):
         """Print the "#" character of the square."""
         if self.__size == 0:
