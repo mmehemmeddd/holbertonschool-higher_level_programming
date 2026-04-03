53d28762636421f59163dbd8ce1a20e7484b3fbb
mehemmeddd <muhammad.mammdv@gmail.com>
2026-04-03 06:35:29 +0000

    task4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This module defines a Square class with size validation."""
+"""This module defines a Square class with getter and setter."""
 
 
 class Square:
@@ -7,11 +7,23 @@ class Square:
 
     def __init__(self, size=0):
         """Initialize the square with a specific size."""
-        if type(size) is not int:
+        # Burada self.size yazırıq ki, birbaşa setter metodunu işə salsın
+        # və yoxlamalardan (if şərtlərindən) keçsin.
+        self.size = size
+
+    @property
+    def size(self):
+        """Retrieve the size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square with validation."""
+        if type(value) is not int:
             raise TypeError("size must be an integer")
-        if size < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        self.__size = value
 
     def area(self):
         """Return the area of the square."""
