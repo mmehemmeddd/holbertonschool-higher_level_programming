6113c52f6f8c1bec6745ca5570678d85e41c6f94
mehemmeddd <muhammad.mammdv@gmail.com>
2026-04-20 16:22:37 +0000

    task7

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -8,4 +8,30 @@ class BaseGeometry:
     """
     Həndəsi fiqurlar üçün baza sinfi.
     """
-    pass
+
+    def area(self):
+        """
+        Sahəni hesablayır.
+        Həmişə Exception qaldırır, çünki hələ tətbiq edilməyib.
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Verilmiş dəyərin düzgün (integer və > 0) olmasını yoxlayır.
+
+        Args:
+            name (str): Dəyərin adı (həmişə string olacağı fərz edilir).
+            value (int): Yoxlanılacaq dəyər.
+
+        Raises:
+            TypeError: value tam ədəd (int) deyilsə.
+            ValueError: value 0-a bərabər və ya kiçikdirsə.
+        """
+        # Strict (qəti) tip yoxlaması
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        
+        # Dəyər yoxlaması
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
