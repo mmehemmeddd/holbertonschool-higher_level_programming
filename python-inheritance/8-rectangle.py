19c3761cb1ffe58851cd4d857e7e6fd6c9780980
mehemmeddd <muhammad.mammdv@gmail.com>
2026-04-20 16:33:48 +0000

    task8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -5,10 +5,15 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """Rectangle class with private width and height."""
+    """A class representing a rectangle, inheriting from BaseGeometry."""
 
     def __init__(self, width, height):
-        """Initialize Rectangle with width and height validated as positive integers."""
+        """
+        Initialize the rectangle with width and height.
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+        """
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
