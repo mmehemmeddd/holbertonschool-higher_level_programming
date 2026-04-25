07889a077f0d8c571fc92bc9f6cb3424ee898de6
mehemmeddd <muhammad.mammdv@gmail.com>
2026-04-25 18:57:27 +0000

    taski bitirdim

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"Salam bu text json ucundur"
+"""Bu modul obyekti JSON formatında mətn faylına yazır."""
 import json
 
 
 def save_to_json_file(my_obj, filename):
-    """Python obyektini (my_obj) JSON formatına salıb fayla (filename) yazır."""
+    """Python obyektini (my_obj) JSON formatına salıb fayla yazır."""
     with open(filename, mode="w", encoding="utf-8") as f:
         json.dump(my_obj, f)
