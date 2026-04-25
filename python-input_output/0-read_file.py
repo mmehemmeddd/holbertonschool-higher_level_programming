2d6369009247a710e23f86a872710dea66831446
mehemmeddd <muhammad.mammdv@gmail.com>
2026-04-25 18:05:33 +0000

    i did succesfully

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
+"""
+Bu modul verilmiş mətn faylını oxumaq və 
+onun məzmununu ekrana yazdırmaq üçün nəzərdə tutulub.
+"""
+
+
 def read_file(filename=""):
-    """Reads a text file (UTF8) and prints it to stdout."""
+    """
+    Bu funksiya faylın adını qəbul edir, onu utf-8 formatında açır
+    və məzmununu olduğu kimi ekrana (stdout) çap edir.
+    """
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
