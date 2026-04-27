a64cab7854c25c072e7c5eed64ee2e78ddd6fb11
mehemmeddd <muhammad.mammdv@gmail.com>
2026-04-27 18:31:46 +0000

    task9

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,14 @@
+-- Cədvəlin yaradılması (əgər artıq mövcud deyilsə)
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
+
+-- Məlumatların (sətirlərin) cədvələ əlavə edilməsi
+INSERT INTO second_table (id, name, score)
+VALUES 
+    (1, 'John', 10),
+    (2, 'Alex', 3),
+    (3, 'Bob', 14),
+    (4, 'George', 8);
