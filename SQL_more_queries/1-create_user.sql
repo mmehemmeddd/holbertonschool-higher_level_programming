bf5b371fcc2423b9658805c3d731fe5467992e76
mehemmeddd <muhammad.mammdv@gmail.com>
2026-04-28 12:39:02 +0000

    task1 bitirdim

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,8 +1,3 @@
--- İstifadəçi mövcud deyilsə, onu 'user_0d_1_pwd' şifrəsi ilə yaradır
+-- Creates the MySQL server user user_0d_1 with all privileges.
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-
--- İstifadəçiyə serverdəki bütün məlumat bazaları və cədvəllər üzərində tam səlahiyyət verir
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-
--- Dəyişikliklərin dərhal qüvvəyə minməsini təmin edir
-FLUSH PRIVILEGES;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
