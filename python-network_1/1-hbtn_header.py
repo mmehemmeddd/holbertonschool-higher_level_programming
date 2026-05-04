27e7fea58848a771fc6963557ce7434110bf043f
mehemmeddd <muhammad.mammdv@gmail.com>
2026-05-04 14:13:43 +0000

    task1

---
+++ python-network_1/1-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+import urllib.request
+import sys
+
+url = sys.argv[1]
+
+with urllib.request.urlopen(url) as response:
+    request_id = response.headers.get('X-Request-Id')
+    print(request_id)
