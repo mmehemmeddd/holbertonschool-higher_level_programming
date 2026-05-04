cb0203e567b69f0aa380a96438da847234190fb5
mehemmeddd <muhammad.mammdv@gmail.com>
2026-05-04 15:07:16 +0000

    task7

---
+++ python-network_1/7-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Handle HTTP errors using requests"""
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    response = requests.get(url)
+
+    if response.status_code >= 400:
+        print("Error code:", response.status_code)
+    else:
+        print(response.text)
