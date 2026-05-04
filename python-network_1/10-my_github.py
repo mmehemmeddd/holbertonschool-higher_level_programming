7a4b780405bf587e8399dc3601a66a651d13c464
mehemmeddd <muhammad.mammdv@gmail.com>
2026-05-04 15:11:31 +0000

    task10

---
+++ python-network_1/10-my_github.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Get GitHub user ID using Basic Auth"""
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    token = sys.argv[2]
+
+    url = "https://api.github.com/user"
+
+    response = requests.get(url, auth=(username, token))
+
+    if response.status_code == 200:
+        print(response.json().get("id"))
+    else:
+        print(None)
